d5d675954c053b44c3f1aff00d8f86b86622721e
magdakolaniak <magda.kolaniak@gmail.com>
2021-03-11 15:54:58 +0100

    ex 6

--- W1-D4.js
+++ W1-D4.js
@@ -75,7 +75,14 @@ strivify ('School')
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
 HINT: Module Operator
 */
-
+const check3and7= function (newNumber) {
+    if (newNumber % 3 ===0 || newNumber % 7 === 0) {
+        console.log("correct number")}
+        else {
+            console.log("wrong number")
+        }
+    }
+check3and7 (25)
 /* WRITE YOUR CODE HERE */
 
 /* EXERCISE 7
